db3117a106912f5675410ff50a3c3bd743b252cf
itsPronay <pronaycoding@gmail.com>
2026-09-10 17:25:45 +0600

    Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.google.ksp) apply false
     alias(libs.plugins.google.gms.google.services) apply false
-    alias(libs.plugins.google.firebase.crashlytics) apply false
     alias(libs.plugins.spotless)
 }
 
