a1d74aa1e5049c75ea728c92e2d8d82656bf4fa4
itsPronay <pronaycoding@gmail.com>
2026-09-24 19:40:42 +0600

    Bump versioncode and versionName

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.pronaycoding.blankee"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.1.3"
+        versionCode = 9
+        versionName = "1.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
